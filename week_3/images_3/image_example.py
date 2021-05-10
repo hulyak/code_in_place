1cc2d4dc534b17f8552379cb62075aeb8dff3c60
hulyak <hulyakarakayaa06@gmail.com>
2021-05-09 20:18:32 -0700

    add exercises

---
+++ week_3/images_3/image_example.py
@@ -0,0 +1,80 @@
+"""
+This program contains several examples of functions that
+manipulate an image to show how the SimpleImage library works.
+"""
+
+from simpleimage import SimpleImage
+
+
+def darker(image):
+    """
+    Makes image passed in darker by halving red, green, blue values.
+    Note: changes in image persist after function ends.
+    """
+    # Demonstrate looping over all the pixels of an image,
+    # changing each pixel to be half its original intensity.
+    for pixel in image:
+        pixel.red = pixel.red // 2
+        pixel.green = pixel.green // 2
+        pixel.blue = pixel.blue // 2
+
+
+def red_channel(filename):
+    """
+    Reads image from file specified by filename.
+    Changes the image as follows:
+    For every pixel, set green and blue values to 0
+    yielding the red channel.
+    Return the changed image.
+    """
+    image = SimpleImage(filename)
+    for pixel in image:
+        pixel.green = 0
+        pixel.blue = 0
+    return image
+
+
+def compute_luminosity(red, green, blue):
+    """
+    Calculates the luminosity of a pixel using NTSC formula
+    to weight red, green, and blue values appropriately.
+    """
+    return (0.299 * red) + (0.587 * green) + (0.114 * blue)
+
+
+def grayscale(filename):
+    """
+    Reads image from file specified by filename.
+    Change the image to be grayscale using the NTSC
+    luminosity formula and return it.
+    """
+    image = SimpleImage(filename)
+    for pixel in image:
+        luminosity = compute_luminosity(pixel.red, pixel.green, pixel.blue)
+        pixel.red = luminosity
+        pixel.green = luminosity
+        pixel.blue = luminosity
+    return image
+
+
+def main():
+    """
+    Run your desired image manipulation functions here.
+    You should store the return value (image) and then
+    call .show() to visualize the output of your program.
+    """
+    flower = SimpleImage('flower.png')
+    flower.show()
+
+    darker(flower)
+    flower.show()
+
+    red_flower = red_channel('flower.png')
+    red_flower.show()
+
+    grayscale_flower = grayscale('flower.png')
+    grayscale_flower.show()
+
+
+if __name__ == '__main__':
+    main()
